e074219e9c396f9d4e28d251407cb243252bb655
Pedro <pedrodmbc@gmail.com>
2019-05-18 11:03:35 -0300

    Init redux store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Provider } from "react-redux";
 
-function App() {
-  return <div className="App">hello</div>;
-}
+import store from "./store";
+
+const App = () => (
+  <Provider store={store}>
+    <h1>Hello World</h1>
+  </Provider>
+);
 
 export default App;
