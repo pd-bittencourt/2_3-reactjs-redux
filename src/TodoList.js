b181f749de4ad08f4b0217bfd60e2cf891e0b6f5
Pedro <pedrodmbc@gmail.com>
2019-05-18 11:54:17 -0300

    Add Todo function with dispatch

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,16 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
+import { dispatch } from "rxjs/internal/observable/range";
 
-const TodoList = ({ todos }) => (
-  <ul>
-    {todos.map(todo => (
-      <li key={todo.id}>{todo.text}</li>
-    ))}
-  </ul>
+const TodoList = ({ todos, addTodo }) => (
+  <>
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+    <button type="submit" onClick={() => addTodo("New text")}>
+      Add
+    </button>
+  </>
 );
 
 TodoList.propTypes = {
+  addTodos: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
@@ -24,4 +31,11 @@ const mapStateToProps = state => ({
   todos: state.todos
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = dispatch => ({
+  addTodo: text => dispatch({ type: "ADD_TODO", payload: { text } })
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
