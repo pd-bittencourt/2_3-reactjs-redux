77f085e386ce6e38f5fc0c7bc89790f3615ae202
Pedro <pedrodmbc@gmail.com>
2019-05-18 13:48:10 -0300

    Organize actions outside component

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as TodoActions from "./store/actions/todos";
 
 const TodoList = ({ todos, addTodo }) => (
   <>
@@ -30,9 +32,8 @@ const mapStateToProps = state => ({
   todos: state.todos
 });
 
-const mapDispatchToProps = dispatch => ({
-  addTodo: text => dispatch({ type: "ADD_TODO", payload: { text } })
-});
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(TodoActions, dispatch);
 
 export default connect(
   mapStateToProps,
