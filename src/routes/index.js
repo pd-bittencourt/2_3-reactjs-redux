3dfcc35b2bb83f31b32f5ccb86b73ddcfeab2b5b
Pedro <pedrodmbc@gmail.com>
2019-05-18 22:36:58 -0300

    Add Footer
    Manipulate and send distant component infos with redux. Components doen't know each other existence!

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from '../pages/main';
+import Footer from '../components/Footer';
 
 const Routes = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Main} />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+      <Footer />
+    </>
   </BrowserRouter>
 );
 
